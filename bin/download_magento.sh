1dfdf5e2fef822b9549dd493b273acaebba3161b
finbertmagestore <finbert@trueplus.vn>
2019-02-19 09:33:31 +0700

    change filename magento source

--- bin/download_magento.sh
+++ bin/download_magento.sh
@@ -11,7 +11,7 @@ MAGENTO_FILENAME='magento/magento2-'${MAGENTO_VERSION}'.tar.gz'
 
 if [[ ${SAMPLE_DATA} = '1' ]]; then
     MAGENTO_DOWNLOAD_URL='http://pubfiles.nexcess.net/magento/ce-packages/magento2-with-samples-'${MAGENTO_VERSION}'.tar.gz'
-    MAGENTO_FILENAME='magento2-with-samples-'${MAGENTO_VERSION}'.tar.gz'
+    MAGENTO_FILENAME='magento/magento2-with-samples-'${MAGENTO_VERSION}'.tar.gz'
 fi
 
 if [[ ! -f  ${MAGENTO_FILENAME} ]]; then
